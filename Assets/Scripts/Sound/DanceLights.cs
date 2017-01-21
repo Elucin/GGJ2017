cebce5b87032eef6f111f278797023e039439301
Andrew Smith <ajsmith22@live.com>
2017-01-21 09:42:40 -0500

    Lights!

--- Assets/Scripts/Sound/DanceLights.cs
+++ Assets/Scripts/Sound/DanceLights.cs
@@ -25,6 +25,15 @@ public class DanceLights : MonoBehaviour {
 
     void Strobe()
     {
-        GameObject.FindObjectOfType<Light>().color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        Light[] lights = GameObject.FindObjectsOfType<Light>();
+        foreach (Light l in lights)
+        {
+            l.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            if(l.name.Contains("Spot"))
+            {
+                int en = Random.Range(0, 2);
+                l.enabled = en == 1;
+            }
+        }
     }
 }
