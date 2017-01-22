182dfe8d3dae96ae26e0d07907a31e4c3a6675d5
Andrew Smith <ajsmith22@live.com>
2017-01-22 15:02:14 -0500

    !

---
+++ Assets/Scripts/Escape.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Escape : MonoBehaviour {
+
+	void Update () {
+	    if(Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+	}
+}
